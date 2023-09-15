fd2f125afd84eab9249950360b919625594a09c2
luisperez9401 <31638158+luisperez9401@users.noreply.github.com>
2023-09-15 07:28:37 -0500

    Update Introduccion.vue

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -10,9 +10,8 @@
 
     figure
       .video
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/aRl9oH1hvuk" title="Administración de medicamentos vía intramuscular en servicios farmacéuticos" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
-    
  
 
 
